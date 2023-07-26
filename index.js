cccb95fa6740b886fc1d89c6a6881c453e6a138f
DiegoGutierrez <diego.alonzo9090@gmail.com>
2023-07-26 18:29:16 -0500

    Se agrego una descripcion pequeña y un boton para el modo oscuro

---
+++ index.js
@@ -0,0 +1,10 @@
+//Funcion para el cambio de color
+function cambiarModoOscuro(){
+    const body = document.body;
+    body.classList.toggle("modo-oscuro");
+}
+
+//Boton para el cambio a modo oscuro
+const botonModoOscuro = document.getElementById("boton-modo-oscuro");
+
+botonModoOscuro.addEventListener("click", cambiarModoOscuro)
