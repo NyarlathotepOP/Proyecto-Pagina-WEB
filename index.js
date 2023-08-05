e797fa98bda4198041a9f45e7c1f104bf3f3da80
Juan Esteban Rodríguez <juanesteban0130@gmail.com>
2023-08-05 17:32:25 -0500

    Se agregan filtros de busqueda avanzada

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ const barraBusqueda = document.getElementById("barra-busqueda");
 
 botonBusqueda.addEventListener("click", () => {
     const busqueda = barraBusqueda.value.toLowerCase();
-    // Aquí puedes agregar la lógica para buscar y mostrar los resultados según la entrada del usuario
+    // Aquí se puede agregar la lógica para buscar y mostrar los resultados según la entrada del usuario
     console.log("Búsqueda realizada:", busqueda);
+});
+const botonAplicarFiltros = document.getElementById("aplicar-filtros");
+const selectCategoria = document.getElementById("categoria");
+const inputPrecio = document.getElementById("precio");
+const seccionProductos = document.querySelector(".productos");
+
+botonAplicarFiltros.addEventListener("click", () => {
+    const categoriaSeleccionada = selectCategoria.value;
+    const precioMaximo = inputPrecio.value !== "" ? parseFloat(inputPrecio.value) : Infinity;
+    
+    // Aquí se puede implementar la lógica para filtrar y mostrar los productos según las opciones seleccionadas
+    
+    seccionProductos.innerHTML = `
+        <div class="producto">Producto 1</div>
+        <div class="producto">Producto 2</div>
+        <div class="producto">Producto 3</div>
+    `;
 });
