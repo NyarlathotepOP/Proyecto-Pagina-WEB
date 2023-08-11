fc8f7e30742aac74a8b38be11b2014e3c7be47c9
DiegoGutierrez <diego.alonzo9090@gmail.com>
2023-08-11 02:47:25 -0500

    Se agrega la funcion para el boton cargar mas

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//modo oscuro-opaco
 function cambiarModoOscuro() {
     const body = document.body;
     body.classList.toggle("modo-oscuro");
@@ -7,3 +8,23 @@ const btnOscuro = document.getElementById("modo-oscuro");
 
 btnOscuro.addEventListener("click", cambiarModoOscuro);
 
+//cargar mas
+const btnCargar = document.getElementById('load-more');
+const productosOcultos = document.querySelectorAll('.box-container .box');
+let productosMostrados = 3;
+const productosPorCargar = 1;
+
+function mostrarProductosOcultos() {
+    for (let i = productosMostrados; i < productosMostrados + productosPorCargar; i++) {
+        if (productosOcultos[i]) {
+            productosOcultos[i].style.display = 'block';
+        }
+    }
+    productosMostrados += productosPorCargar;
+    //Oculta el boton
+    if (productosMostrados >= productosOcultos.length) {
+        btnCargar.style.display = 'none';
+    }
+}
+btnCargar.addEventListener('click', mostrarProductosOcultos);
+mostrarProductosOcultos();
